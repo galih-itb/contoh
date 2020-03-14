e81f17ea558d509045b187e382ad2efa00f883e3
Galih Hermawan <61464275+galih-itb@users.noreply.github.com>
2020-03-14 18:59:07 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    render_template('index.html')
+    return render_template('index.html')
     #return "Galih Hermawan"
 
 @application.route('/user/<nama>')
